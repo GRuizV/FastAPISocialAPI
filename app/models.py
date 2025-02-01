fc90a2546b5c25ad4d0bf41e85dcc69da4aceaaa
Gerardo Ruiz <grville93@gmail.com>
2025-02-01 00:37:48 -0500

    models.py file added to manage the migrations

---
+++ app/models.py
@@ -0,0 +1,42 @@
+# 3RD PARTY IMPORTS
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+
+# LOCAL IMPORTS
+from .database import Base
+
+
+class Post(Base):
+
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key = True, nullable = False)
+    title = Column(String, nullable = False)
+    content = Column(String, nullable = False)
+    published = Column(Boolean, nullable = False, server_default = 'True')
+    created_at = Column(TIMESTAMP(timezone=True), nullable = False, server_default = 'NOW()')
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
