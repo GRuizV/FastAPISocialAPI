0115200726a0f3a024eb7e9ff12f7578b440426c
Gerardo Ruiz <grville93@gmail.com>
2025-02-05 23:02:47 -0500

    utils.py file added to manage some misc functions

---
+++ app/utils.py
@@ -0,0 +1,37 @@
+
+# 3RD PARTY IMPORTS
+from passlib.context import CryptContext
+
+# LOCAL IMPORTS
+# from . import schemas
+
+# BUILT-IN IMPORTS
+# import logging
+
+
+
+# Passlib algorithm setting
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+# Initial Hash Function
+def hash(password:str) -> str:
+
+    """This function has a password when a new user is created"""
+
+    return pwd_context.hash(password)
+
+
+# Comparator of passwords
+def compare_pwds(plain_pwd:str, hashed_pwd:str) -> bool:
+
+    """This function compares two passwords to see if they match according to the hash algorithm"""
+
+    return pwd_context.verify(plain_pwd, hashed_pwd)
+
+
+
+
+
+
+
