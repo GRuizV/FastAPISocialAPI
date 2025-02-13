7df29a61c5e54d17a5f92ec79137891b30244aa1
Gerardo Ruiz <grville93@gmail.com>
2025-02-12 20:17:54 -0500

    The login endpoint was configured to now retrieve a Token after the authentication

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,15 +1,16 @@
 
 # 3RD PARTY IMPORTS
-from fastapi import APIRouter, status, HTTPException, Depends, Response
+from fastapi import APIRouter, status, HTTPException, Depends
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 # LOCAL IMPORTS
-from app import schemas, utils, models
+from app import schemas, utils, models, oauth2
 from app.database import get_db
 
 
 # BUILT-IN IMPORTS
-from typing import List
+...
 
 
 
@@ -22,14 +23,14 @@ router = APIRouter(tags=["Authentication"])
 
 # LOGIN PATH OP
 @router.post('/login', status_code=status.HTTP_202_ACCEPTED)
-def login(user_credentials: schemas.UserLogin, db: Session = Depends(get_db)):
+def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     
     # Build the DB query
-    user_query = db.query(models.User).filter(models.User.email == user_credentials.email)
+    user_query = db.query(models.User).filter(models.User.email == user_credentials.username)
 
     # User existence validation query execution guard
     if not user_query.first():
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"The user with email '{user_credentials.email}' was not found!") 
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid Credentials!") 
 
     # Get the actual authorized user
     user = user_query.first()
@@ -38,10 +39,10 @@ def login(user_credentials: schemas.UserLogin, db: Session = Depends(get_db)):
     if not utils.compare_pwds(user_credentials.password, user.password):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f"Invalid Credentials!") 
     
-    # Created token
-    # Return the token
+    # Create the token
+    access_token = oauth2.create_access_token(data= {"user_id": user.id})
 
-    return {"message": f"Welcome, {user_credentials.email}. You have successfully logged in!"}
+    return {"token": access_token, "token_type": "bearer"}
 
 
 
