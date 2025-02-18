c92571cb2ac07444b0e6bb03e49f2dc5cdb20b57
Gerardo Ruiz <grville93@gmail.com>
2025-02-18 00:02:37 -0500

    Relationship with the entity -User- established for posts

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 # 3RD PARTY IMPORTS
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
 from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.sql import func
 
 # LOCAL IMPORTS
 from .database import Base
@@ -17,10 +19,10 @@ class Post(Base):
     title = Column(String, nullable = False)
     content = Column(String, nullable = False)
     published = Column(Boolean, nullable = False, server_default = 'True')
-    created_at = Column(TIMESTAMP(timezone = True), nullable = False, server_default = 'NOW()')
+    created_at = Column(TIMESTAMP(timezone = True), nullable = False, server_default = func.now())
     owner_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE", onupdate="CASCADE"), nullable = False)
     
-
+    owner = relationship("User")
 
 
 # Users DB Model Setting
@@ -31,7 +33,7 @@ class User(Base):
     id = Column(Integer, primary_key = True, nullable = False)
     email = Column(String, nullable = False, unique = True)
     password = Column(String, nullable = False)
-    created_at = Column(TIMESTAMP(timezone=True), nullable = False, server_default = 'NOW()')
+    created_at = Column(TIMESTAMP(timezone=True), nullable = False, server_default = func.now())
     
 
 
