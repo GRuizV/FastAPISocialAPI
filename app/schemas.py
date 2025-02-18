7609d50f999672a8178007083506fb275609afb4
Gerardo Ruiz <grville93@gmail.com>
2025-02-18 00:03:51 -0500

    Pydantic Schemas restructured to be able to retrive the actual user alongside the post when returned in the respective path Post operations

--- app/schemas.py
+++ app/schemas.py
@@ -14,38 +14,6 @@ from typing import Optional
 
 
 
-# POST MODEL SCHEMAS
-class PostBase(BaseModel):
-
-    title: str
-    content: str
-    published: bool = True
-    owner_id: int
-
-
-class PostCreate(PostBase):
-
-    """
-    We are leaving this schema as placeholder in case the creation of a new post requires
-    something specific additional to the PostBase    
-    """
-
-    pass
-
-
-class PostResponse(PostBase):
-
-    id: int
-    owner_id: int
-    created_at: datetime
-
-    # Pydantic setting to convert this class to a dict like response even when is not one
-    class Config:
-        from_attributes = True
-
-
-
-
 # USER MODEL SCHEMAS
 class UserBase(BaseModel):
 
@@ -96,3 +64,32 @@ class TokenData(BaseModel):
 
 
 
+
+# POST MODEL SCHEMAS
+class PostBase(BaseModel):
+
+    title: str
+    content: str
+    published: bool = True
+
+
+class PostCreate(PostBase):
+
+    """
+    We are leaving this schema as placeholder in case the creation of a new post requires
+    something specific additional to the PostBase    
+    """
+
+    pass
+
+
+class PostResponse(PostBase):
+
+    id: int
+    owner_id: int
+    created_at: datetime
+    owner: UserResponse
+
+    # Pydantic setting to convert this class to a dict like response even when is not one
+    class Config:
+        from_attributes = True
